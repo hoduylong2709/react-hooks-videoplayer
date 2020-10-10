8d3947836aaafbebee9bc8b1f4632ff4e4cc3db2
longho <hoduylong411@gmail.com>
2020-10-10 15:33:10 +0700

    Setting up the routes

--- src/components/containers/App.js
+++ src/components/containers/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import HdlPlayer from './HdlPlayer';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 const App = () => (
-  <HdlPlayer />
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={HdlPlayer} />
+      <Route exact path="/" component={HdlPlayer} />
+    </Switch>
+  </BrowserRouter>
 )
 
 export default App;
